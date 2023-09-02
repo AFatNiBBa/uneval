2cb5879a4717fb4622e4cd8f111619084fac991c
Sean Alunni <seanalunni@gmail.com>
2023-09-02 07:18:23 +0200

    BACKUP

--- package/test/src/proxy.test.ts
+++ package/test/src/proxy.test.ts
@@ -3,3 +3,9 @@ import { check } from "./util";
 import "@seanalunni/uneval-v8";
 
 check("normal", new Proxy({ a: 1 }, { get() {} }), "new Proxy({a:1},{get() {}})");
+
+// test("circular", () => {
+//     const a: any = { c: 1 };
+//     a.b = new Proxy(a, <any>{ b: 2 });
+//     console.log(">>>", serialize(a, true));
+// });
--- package/test/src/util.ts
+++ package/test/src/util.ts
@@ -1,7 +1,7 @@
 
-import uneval, { Opts } from "uneval.js";
+import uneval from "uneval.js";
 
-export const serialize = (x: any, opts?: Opts) => uneval(x, Object.assign({ pretty: false }, opts));
+export const serialize = (x: any, pretty = false) => uneval(x, { pretty, tab: 2 });
 export const sanitize = (x: string) => x.replace(/\{\s+\}/g, "{}"); // Ensures that the compiler doesn't change the source of functions to much
 export const check = (name: string, x: any, str: string) => test(name, () => expect(sanitize(serialize(x))).toBe(str));
 export const back = <T>(x: T) => <T>(0,eval)(serialize(x));
